82a6f432ee4fd72aaa5183610718cab6a9f1912b
Liakuria <04kuria45@gmail.com>
2026-09-13 18:38:10 +0900

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -8,11 +8,17 @@ const quizData = [
     answer: 1 // 正解は2番目の「ジゴク」
   },
 
+  {
+    type: "choice",
+    levels: ["easy", "normal"],
+    question: "宵闇めぇの誕生日は？",
+    options: ["3/18","3/2","3/24","3/30"],
+    answer: 0
   // 【入力問題の例】
   {
     type: "input",
     levels: ["easy", "normal"],
-    question: "GΔ59の正しい読み方をひらがなで入力してください。",
+    question: "GΔ59の正しい読み方をで入力してください。",
     // 正解の表記揺れ（全角・半角・カタカナ等）を配列で複数登録できます
     answers: ["じごく", "ジゴク", "jigoku"] 
   },
@@ -20,7 +26,7 @@ const quizData = [
   {
     type: "input",
     levels: ["normal", "hard"],
-    question: "「幸せを運ぶお稲荷さま」の名前は何？（苗字なし・ひらがな/カタカナ可）",
+    question: "「幸せを運ぶお稲荷さま」の名前は何？",
     answers: ["甘噛こんこん", "あまがみこんこん", "こんこん"]
   }
 ];
